9c844ef44a79d692d75b8eb9a56da316515d5ec7
Ben Cavins <ben.cavins@flatironschool.com>
2023-01-18 16:59:55 -0500

    finish blood

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 // let firstMovie;
+let moviesData;
 
 fetch('http://localhost:3000/movies')
     .then(resp => resp.json())
     .then(moviesArray => {
+        moviesData = moviesArray
         moviesArray.forEach((movieObj) => {
             // loop code goes here
             const myImg = document.createElement('img')
             myImg.src = movieObj.image
             myImg.alt = movieObj.title
+            myImg.addEventListener('click', (event) => {
+                renderMovieDetails(movieObj)
+            })
+
             document.querySelector('#movie-list').append(myImg)
             // const movieList = document.querySelector('#movie-list')
             // movieList.append(myImg)
@@ -16,6 +22,21 @@ fetch('http://localhost:3000/movies')
         renderMovieDetails(moviesArray[0])
         // return moviesArray[0]  // or return it to the next .then()
     })
+    .then(() => doBloodForm())
+
+function doBloodForm() {
+    const bloodForm = document.querySelector('#blood-form')
+    bloodForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const bloodInput = document.querySelector('#blood-amount')
+        console.log(bloodInput.value)
+
+        const currentBloodAmount = parseInt(
+            document.querySelector('#amount').textContent
+        )
+        document.querySelector('#amount').textContent = currentBloodAmount + parseInt(bloodInput.value)
+    })
+}
 
 function renderMovieDetails(movie) {
     const detailImage = document.querySelector('#detail-image')
@@ -28,5 +49,22 @@ function renderMovieDetails(movie) {
 
     document.querySelector('#description').textContent = movie.description
 
-    document.querySelector('#amount').textContent = movie.blood_amount
+    // document.querySelector('#amount').textContent = movie.blood_amount
+
+    const watchedBtn = document.querySelector('#watched')
+    watchedBtn.addEventListener('click', (event) => {
+        console.log('clicked')
+        movie.watched = !movie.watched
+        if (movie.watched) {
+            watchedBtn.textContent = 'Watched'
+        } else {
+            watchedBtn.textContent = 'Unwatched'
+        }
+    })
+
+    if (movie.watched) {
+        watchedBtn.textContent = 'Watched'
+    } else {
+        watchedBtn.textContent = 'Unwatched'
+    }
 }
